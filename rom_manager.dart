05f80f04468d5e5dab8d76f83a47961418e9cae9
phakphoum38-stack <phakphoum38@gmail.com>
2026-03-19 09:30:22 +0700

    Create rom_manager.dart

---
+++ rom_manager.dart
@@ -0,0 +1,13 @@
+import 'package:file_picker/file_picker.dart';
+
+Future<String?> pickROM() async {
+
+  FilePickerResult? result =
+      await FilePicker.platform.pickFiles();
+
+  if (result != null) {
+    return result.files.single.path;
+  }
+
+  return null;
+}
